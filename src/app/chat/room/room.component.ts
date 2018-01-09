715d98b97273c8353af3e44f07ca732e709bd1d0
Andrii Mozhlianskyi <mozhlyanskiy1993@gmail.com>
2018-01-09 22:29:14 +0200

    show room title

--- src/app/chat/room/room.component.ts
+++ src/app/chat/room/room.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { MessagesService } from '../messages/messages.service';
 import { UsersService } from '../users/users.service';
+import { RoomsService } from '../rooms/rooms.service';
+import { AppStore } from '../../app.store';
+import { RoomState } from './room.state';
 
 @Component({
   selector: 'app-room',
@@ -8,13 +13,23 @@ import { UsersService } from '../users/users.service';
   styleUrls: ['./room.component.css'],
   providers: [
     MessagesService,
+    RoomsService,
     UsersService
   ]
 })
 export class RoomComponent implements OnInit {
+  room: RoomState;
 
-  constructor () { }
+  constructor (
+    @Inject(AppStore) private store,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => this.room = this.getRoom(+paramMap.get('id')));
+  }
+
+  private getRoom (id: number): RoomState {
+    return this.store.getState().chat.rooms.find(room => room.id === id);
   }
 }
