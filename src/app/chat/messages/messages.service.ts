5d0db9f7d3c5eebd0a47343432e0deaa0a44fd13
Andrii Mozhlianskyi <mozhlyanskiy1993@gmail.com>
2018-02-12 21:43:39 +0200

    replace room id mock with route param

--- src/app/chat/messages/messages.service.ts
+++ src/app/chat/messages/messages.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Resolve } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import 'rxjs/add/observable/of';
 
 import { MessageState } from './message.state';
@@ -11,7 +11,7 @@ export class MessagesService implements Resolve<MessageState[]> {
 
   constructor (private http: HttpClient) {}
 
-  resolve (): Observable<MessageState[]> {
-    return this.http.get<MessageState[]>('api/rooms/1/messages');
+  resolve (route: ActivatedRouteSnapshot): Observable<MessageState[]> {
+    return this.http.get<MessageState[]>(`api/rooms/${route.params.id}/messages`);
   }
 }
--- src/app/chat/users/users.service.ts
+++ src/app/chat/users/users.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Resolve } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import 'rxjs/add/observable/of';
 
 import { UserState } from './user.state';
@@ -11,7 +11,7 @@ export class UsersService implements Resolve<UserState[]> {
 
   constructor (private http: HttpClient) { }
 
-  resolve (): Observable<UserState[]> {
-    return this.http.get<UserState[]>('api/rooms/1/users');
+  resolve (route: ActivatedRouteSnapshot): Observable<UserState[]> {
+    return this.http.get<UserState[]>(`api/rooms/${route.params.id}/users`);
   }
 }
